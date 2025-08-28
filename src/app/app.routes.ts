7595736d61e9ee82fc5e15df1eb32e098c0d5297
Merk42 <mark_c@markecurtis.com>
2025-08-28 13:50:30 -0400

    homepage layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { Examples } from './examples/examples';
 import { Homepage } from './homepage/homepage';
 export const routes: Routes = [
-  { path: '', component: Examples},
+  { path: '', component: Homepage},
   { path: 'library', component: Examples},
 ];
--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { AnimatedBox } from '../library/animated-box/animated-box';
 import { AnimatedData } from '../library/animated-data/animated-data';
+import { Card } from '../library/card/card';
+import { Carousel } from '../library/carousel/carousel';
+import { CarouselConfig } from '../library/carousel/carousel-config';
+import { CarouselContent } from '../library/carousel/carousel-content/carousel-content';
 import { EvenGrid } from '../library/even-grid/even-grid';
+import { SummaryLists } from '../library/summary-lists/summary-lists';
+import { SummaryList } from '../library/summary-lists/summary-list/summary-list';
 
 @Component({
   selector: 'qts-homepage',
-  imports: [AnimatedBox, AnimatedData, EvenGrid],
+  imports: [AnimatedBox, AnimatedData, Card, Carousel, CarouselContent, EvenGrid, SummaryList, SummaryLists],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
 export class Homepage {
-
+  carouselconfig: CarouselConfig = new CarouselConfig({small:2.25,medium:3,large:3, xlarge:3, xxlarge:3});
 }
